2522452b1da950a491179c5d2b42224f359038b9
Nahid Hasan <63138743+nahidhasan141400@users.noreply.github.com>
2024-10-24 16:25:01 +0600

    update auth.ts

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,10 +1,11 @@
+import { AdministrationController } from "@/controllers/Administration/Administration.controller";
 import IsUser from "@/middleware/auth/isUser";
 import CreateRouter from "@CreateRoute";
 
 // create registration route
-const MakeRouter = new CreateRouter("/ui/auth");
+const MakeRouter = new CreateRouter("/ui/admin/auth");
 const app = MakeRouter.getApp();
 
-// mobile app api
+app.post("/login", AdministrationController.Login);
 
 export default MakeRouter;
